a4e75ba3eaba13ea08befcd8bee0c77892316663
Elias F. Fyksen <git@fyksen.net>
2023-07-09 21:33:21 +0100

    Remove old ast

--- src/ast_old/scope/mod.rs
+++
@@ -1,7 +0,0 @@
-mod scope;
-
-pub use scope::Scopable;
-pub use scope::Scope;
-pub use scope::Entry;
-pub use scope::Function;
-pub use scope::Variable;
--- src/ast_old/stmt/mod.rs
+++
@@ -1,8 +0,0 @@
-mod block;
-mod if_stmt;
-mod stmt;
-
-pub use block::Block;
-pub use if_stmt::If;
-pub use stmt::Stmt;
-pub use stmt::VarDecl;
